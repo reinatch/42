5c2b08bb64aa41c0a5d11c90488274a6a865d73f
rerodrig <rerodrig@c1r9s9.42porto.com>
2023-05-17 15:48:55 +0100

    v003bonus

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: rerodrig <rerodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 13:33:54 by rerodrig          #+#    #+#             */
-/*   Updated: 2023/05/17 14:00:23 by rerodrig         ###   ########.fr       */
+/*   Updated: 2023/05/17 15:44:20 by rerodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,4 +56,23 @@ void ft_putchar_fd(char c, int fd);
 void ft_putstr_fd(char *s, int fd);
 void ft_putendl_fd(char *s, int fd);
 void ft_putnbr_fd(int n, int fd);
+
+//3rd
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+
+}t_list;
+
+t_list *ft_lstnew(void *content);
+void ft_lstadd_front(t_list **lst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
+void ft_lstadd_back(t_list **lst,t_list *new);
+void ft_lstdelone(t_list *lst, void (*del)(void*));
+void ft_lstclear(t_list **lst, void (*del)(void*));
+void ft_lstiter(t_list *lst, void (*f)(void *));
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *));
+
 #endif
